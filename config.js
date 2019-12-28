c241aff59088f66ca413c28723d084544dc7db28
Evylon <mail@evylon.de>
2019-12-28 16:38:26 +0100

    add rules, map, initial state and draft for config

---
+++ config.js
@@ -0,0 +1,36 @@
+const config = {
+    cardCount: 137,
+    CorporationCount: 10,
+    tilesCount: 61,
+    cardTypes: [
+        'Automated', 'Active', 'Event', 'Corporation'
+    ],
+    requirements: [
+        'Temperature', 'Oxygen', 'Oceans', 'Tag', 'Production'
+    ],
+    tags: [
+        'Science,', 'Building', 'Space', 'Microbe', 'Plant', 'Animal',
+        'City', 'Earth', 'Jovian', 'Energy', 'Venus', 'Event'
+    ],
+    resources: [
+        'Megacredit', 'Steel', 'Titanium', 'Plant', 'Energy', 'Heat'
+    ],
+    globalStates: [
+        'Temperature', 'Oceans', 'Oxygen', 'Terraforming Rating', 'Generation'
+    ],
+    tiles: [
+        'City', 'Ocean', 'Greenery', 'Special'
+    ],
+    specialTiles: [
+        'vulcan', 'nuclear explosion', 'Mine'
+    ],
+    extraTiles: [
+        'Phobos Space Haven', 'Ganymede Colony'
+    ],
+    milestones: [
+        'Terraformer', 'Mayor', 'Gardener', 'Builder', 'Planner'
+    ],
+    awards: [
+        'Landlord', 'Banker', 'Scientist', 'Thermalist', 'Miner'
+    ]
+}
