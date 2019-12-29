fd13a1f632a72ef8b4233077c5b61ea4a3e0cda6
Evylon <chaos@evylon.de>
2019-12-30 00:26:17 +0100

    load card list from hardcoded string instead of std args

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
-use std::io;
+use std::env;
 use std::process;
 
 // The `main` function is where your program starts executing.
 fn main() {
     // Create a CSV parser that reads data from stdin.
-    let mut rdr = csv::Reader::from_reader(io::stdin());
+    let mut path = env::current_dir().expect("current working directory");
+    path.push("card_list.csv");
+    let mut rdr = csv::Reader::from_path(path).expect("csv reader");
     let columns = rdr.headers().expect("columns of card list");
     println!("{:?}", columns);
     process::exit(0);
-
-    // Loop over each record.
-    for result in rdr.records() {
-        // An error may occur, so abort the program in an unfriendly way.
-        // We will make this more friendly later!
-        let record = result.expect("a CSV record");
-        // Print a debug version of the record.
-        println!("{:?}", record);
-    }
 }
