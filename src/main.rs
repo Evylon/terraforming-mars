fbaf96a70a67411955c12a900a4ce6e528e6daea
Evylon <chaos@evylon.de>
2019-12-29 23:55:48 +0100

    add rust environment

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::io;
+use std::process;
+
+// The `main` function is where your program starts executing.
+fn main() {
+    // Create a CSV parser that reads data from stdin.
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+    let columns = rdr.headers().expect("columns of card list");
+    println!("{:?}", columns);
+    process::exit(0);
+
+    // Loop over each record.
+    for result in rdr.records() {
+        // An error may occur, so abort the program in an unfriendly way.
+        // We will make this more friendly later!
+        let record = result.expect("a CSV record");
+        // Print a debug version of the record.
+        println!("{:?}", record);
+    }
+}
